1d00e369de75188b416037b45ecfd98f42fe29e4
Wonju Jo <68497276+wonju-cho@users.noreply.github.com>
2024-12-04 20:22:51 +0900

    Title: 합을 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241204/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int tempA = 1, tempB = 2, tempC = 3;
+    int a = tempA + tempB + tempC;
+    int b = a; int c = a;
+    cout<<a<<" "<<b<<" "<<c;
+    return 0;
+}
